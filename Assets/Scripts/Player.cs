1f13248ab50be46bb7f4dc8f66841b2beb84ff64
Kris Camp <rikscamp@gmail.com>
2022-01-29 15:47:43 -0500

    controls
    basic controls, cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Controls controls;
 
+    public float speed;
+    public float turnSpeed;
 
+    public Vector2 controllerInput;
+    private void Awake()
+    {
+        controls = new Controls();
+
+        controls.Racing.Move.performed += ctx => controllerInput = ctx.ReadValue<Vector2>();
+        controls.Racing.Move.canceled += ctx => controllerInput = Vector2.zero;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +25,28 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
+
+        if (controllerInput.x >= 0.2f)
+        {
+            transform.Translate(Vector2.right * Time.fixedDeltaTime * turnSpeed);
+        }
+
+        if (controllerInput.x <= -0.2f)
+        {
+            transform.Translate(Vector2.left * Time.fixedDeltaTime * turnSpeed);
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Racing.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Racing.Disable();
     }
 }
