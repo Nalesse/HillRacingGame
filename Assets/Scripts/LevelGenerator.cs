88d5be06882490c2b45472edfcc2ab5bfb56a435
Nalesse <nalesse@student.fitchburgstate.edu>
2022-01-31 15:51:35 -0500

    Added ability to tilt road

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -15,31 +16,28 @@ public class LevelGenerator : MonoBehaviour
     private Collider col;
     private GameObject spawnedTile;
     private Vector3 nextSpawnPoint;
+    private GameObject roadTileContainer;
     #endregion
 
     #region Inspector Fields
 
     [SerializeField] private GameObject roadTile;
     [SerializeField] private int tilesToPreSpawn;
-
+    [SerializeField] private float roadSlopeAngle;
     #endregion
 
-
+    
+    
     public void SpawnTile()
     {
+        //Spawns a tile and sets the parent to the container. Then preforms setup for the next tile to be spawned
         spawnedTile = Instantiate(roadTile, nextSpawnPoint, Quaternion.identity);
+        spawnedTile.transform.SetParent(roadTileContainer.transform, false);
         col = spawnedTile.GetComponentInChildren<BoxCollider>();
         nextSpawnPoint = CalculateNextSpawnPoint();
     }
-
-    private void Start()
-    {
-        for (int i = 0; i < tilesToPreSpawn; i++)
-        {
-            SpawnTile();
-        }
-    }
-
+    
+    
     private void Awake()
     {
         // Singleton setup
@@ -52,15 +50,38 @@ public class LevelGenerator : MonoBehaviour
             Instance = this;
         }
     }
+    
+    private void Start()
+    {
+        roadTileContainer = new GameObject
+        {
+            name = "RoadTileContainer"
+        };
 
+        for (int i = 0; i < tilesToPreSpawn; i++)
+        {
+            SpawnTile();
+        }
+        // Sets the rotation of the container, which also rotates the tiles
+        SetRotation(roadTileContainer, roadSlopeAngle, Vector3.right);
+    }
+
+    
+    // Helper Functions
     private Vector3 CalculateNextSpawnPoint()
     {
-        Vector3 currentTilePosition = spawnedTile.transform.position;
-
-        nextSpawnPoint.x = currentTilePosition.x;
-        nextSpawnPoint.y = currentTilePosition.y;
+        // localPosition is used so that the tiles position is local to that of the container
+        Vector3 currentTilePosition = spawnedTile.transform.localPosition;
+        
         nextSpawnPoint.z = currentTilePosition.z + col.bounds.size.z;
 
         return nextSpawnPoint;
+    }
+
+    private void SetRotation(GameObject objectToRotate, float angle, Vector3 direction)
+    {
+        objectToRotate.transform.rotation = Quaternion.AngleAxis(angle, direction);
+        
+
     }
 }
