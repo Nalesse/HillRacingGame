b4110ab1127c602066bb0136ffbbe5cdc73d6cc3
Nalesse <nalesse@student.fitchburgstate.edu>
2022-03-03 15:14:27 -0500

    Progress on customizable randomness

--- Assets/Scripts/Spawnables/Spawnable.cs
+++ Assets/Scripts/Spawnables/Spawnable.cs
@@ -12,6 +12,22 @@ namespace Spawnables
         public Vector3 positionOffset;
         public GameObject prefab;
         public float chanceToSpawn;
+
+        [Header("Randomness")]
+        public float minX, maxX;
+        public float minY, maxY;
+        public float minZ, maxZ;
         #endregion
+
+        public Vector3 GenerateRandomPosition()
+        {
+            var randomPos = new Vector3();
+
+            randomPos.x = UnityEngine.Random.Range(minX, minX);
+            randomPos.y = UnityEngine.Random.Range(minY, maxY);
+            randomPos.z = UnityEngine.Random.Range(minZ, maxZ);
+
+            return randomPos;
+        }
     }
 }
