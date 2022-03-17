e5cd2df95fc30619abe40bf5c8758a9365d6f55e
Nalesse <nalesse@student.fitchburgstate.edu>
2022-03-17 16:17:43 -0400

    More progress on cars

--- Assets/Scripts/ShityCar.cs
+++ Assets/Scripts/ShityCar.cs
@@ -5,29 +5,27 @@ using UnityEngine;
 public class ShityCar : MonoBehaviour
 {
     Rigidbody carRB;
+
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
         carRB = GetComponent<Rigidbody>();
         transform.parent = null;
+        speed = Player.Instance.speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         var distance = (Player.Instance.transform.position - transform.position).z;
-        var speed = Player.Instance.speed;
+        
 
-        if(distance >= 20)
+        Debug.Log(distance);
+        if(distance >= -100)
         {
-            Debug.Log(distance);
             var currentVelocity = carRB.velocity.y;
             carRB.velocity = new Vector3(0, currentVelocity, 1 * speed * Time.deltaTime);
-        }
-        else if(distance >= 300)
-        {
-            Destroy(gameObject, 2);
-            Debug.Log("Car destroyed");
         }
     }
 }
