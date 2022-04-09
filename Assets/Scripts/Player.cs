bded244729a07b2cbb8e7ed6261553e1504036ee
rikscamp <48299053+rikscamp@users.noreply.github.com>
2022-04-09 18:16:19 -0400

    minor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -239,6 +239,8 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(TrickSystem.CooldownTrick("Strick"));
         }
+
+
     }
 
     #endregion
@@ -272,6 +274,10 @@ public class Player : MonoBehaviour
         {
             TrickSystem.isDoingTrick = false;
             StartCoroutine(Damage());
+
+
+
+
         }
     }
     
@@ -348,7 +354,7 @@ public class Player : MonoBehaviour
     }
 
      IEnumerator Damage()
-    {
+     {
          Debug.Log("Wipeout");
          
          if (TrickSystem.animatorBool != String.Empty)
@@ -361,7 +367,9 @@ public class Player : MonoBehaviour
          yield return new WaitForSeconds(4);
          isDamage = false;
 
-    }
+        
+
+     }
      
 
     private void OnCollisionEnter(Collision collision)
