033fdf48f54d9bc5cfd0240c33bb5a2651cd3546
FSU-MCC <mcampb40@student.fitchburgstate.edu>
2022-04-14 15:15:49 -0400

    Options stuff

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        string option = 1920 + " x " + 1080;
+        options.Add(option);
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
