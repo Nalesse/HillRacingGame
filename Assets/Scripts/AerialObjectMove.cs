48fa0b5e67c8c743beab90043c796aa76a677e1e
Ryan Normand <rnorman6@student.fitchburgstate.edu>
2022-04-16 20:41:22 -0400

    THE BALLOONS

--- Assets/Scripts/AerialObjectMove.cs
+++ Assets/Scripts/AerialObjectMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AerialObjectMove : MonoBehaviour
 {
     public float speed = 40.0f;
+    public float minDistance;
+    public float minDestroyDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,23 @@ public class AerialObjectMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void FixedUpdate()
+    {
+        var distance = (Player.Instance.transform.position - transform.position).z;
+
+        // Starts moving the car when the player is within the min distance
+        if (distance >= minDistance)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (distance >= minDestroyDistance)
+        {
+            Destroy(gameObject);
+        }
+
+
     }
 }
