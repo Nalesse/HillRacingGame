8a88c6e6813d9d4d8e955983030e446b292eb758
rnorman6 <79875112+rnorman6@users.noreply.github.com>
2022-04-28 15:05:06 -0400

    updated main menu with *flavor*
    added in moving cars, moving birds, and pedestrians to main menu

---
+++ Assets/Scripts/Misc/BirdMoving.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMoving : MonoBehaviour
+{
+    public float speed = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (transform.position.z < -50)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
