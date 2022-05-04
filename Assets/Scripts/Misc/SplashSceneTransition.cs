4f79c4106e1cbed3d030f5aef90093fe05b3c68e
Bryophite1 <76822050+Bryophite1@users.noreply.github.com>
2022-05-03 21:57:16 -0400

    Splash Screen

---
+++ Assets/Scripts/Misc/SplashSceneTransition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SceneChanger());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator SceneChanger() 
+    {
+        yield return new WaitForSeconds(17.0f);
+        SceneManager.LoadScene(1);
+    }
+}
